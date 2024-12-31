56b284cfd256bfbae2d46d5ad4588212fbfcb4d2
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-12-31 05:20:59 -0600

    refactored the whole world

--- trs80coco/screen0/world/world.lua
+++ trs80coco/screen0/world/world.lua
@@ -9,60 +9,21 @@ local inventories = require("world.inventories")
 
 local M = {}
 
-function M.get_character(character_id)
-    local character_data = data.characters[character_id]
-
-    local character = {}
-    character.character_id = character_id
-
-    characters.construct(M, character, character_data)
-
-    return character
+function M.initialize()
+    M.abandon()
+    initializer.initialize(M)
 end
 
-function M.get_room(room_id)
-    local room_data = data.rooms[room_id]
-
-    local room = {}
-    room.room_id = room_id
-
-    rooms.construct(M, room, room_data)
-
-    return room
+function M.abandon()
+    data.rooms = {}
+    data.characters = {}
+    data.factions = {}
+    data.messages = {}
+    data.inventories = {}
+    data.avatar_id = nil
 end
 
-function M.get_inventory(inventory_id)
-    local inventory_data = data.inventories[inventory_id]
-
-    local inventory = {}
-    inventory.inventory_id = inventory_id
-
-    inventories.construct(M, inventory, inventory_data)
-
-    return inventory
-end
-
-function M.create_inventory()
-    local inventory_id = #(data.inventories) + 1
-    data.inventories[inventory_id] = {}
-
-    local inventory = M.get_inventory(inventory_id)
-    return inventory
-end
-
-function M.create_room(description)
-    local room_id = #(data.rooms) + 1
-    data.rooms[room_id] = {}
-    
-    local room_data = data.rooms[room_id]
-    room_data.description = description
-    room_data.routes={}
-    room_data.characters={}
-
-    local room = M.get_room(room_id)
-    room:set_inventory(M.create_inventory())
-    return room
-end
+--CHARACTERS-------------------------------------------------------------------
 
 function M.create_character(room, faction, character_type)
     local character_id = nil
@@ -84,16 +45,70 @@ function M.create_character(room, faction, character_type)
     return result
 end
 
-function M.get_faction(faction_id)
-    local faction_data = data.factions[faction_id]
+function M.get_character(character_id)
+    local character_data = data.characters[character_id]
 
-    local faction = {}
-    faction.faction_id = faction_id
+    local character = {}
+    character.character_id = character_id
 
-    factions.construct(M, faction, faction_data)
+    characters.construct(M, character, character_data)
 
-    return faction
+    return character
 end
+
+function M.recycle_character(character)
+    data.characters[character.character_id] = nil
+end
+
+--ROOMS------------------------------------------------------------------------
+
+function M.create_room(description)
+    local room_id = #(data.rooms) + 1
+    data.rooms[room_id] = {}
+    
+    local room_data = data.rooms[room_id]
+    room_data.description = description
+    room_data.routes={}
+    room_data.characters={}
+
+    local room = M.get_room(room_id)
+    room:set_inventory(M.create_inventory())
+    return room
+end
+
+function M.get_room(room_id)
+    local room_data = data.rooms[room_id]
+
+    local room = {}
+    room.room_id = room_id
+
+    rooms.construct(M, room, room_data)
+
+    return room
+end
+
+--INVENTORIES------------------------------------------------------------------
+
+function M.create_inventory()
+    local inventory_id = #(data.inventories) + 1
+    data.inventories[inventory_id] = {}
+
+    local inventory = M.get_inventory(inventory_id)
+    return inventory
+end
+
+function M.get_inventory(inventory_id)
+    local inventory_data = data.inventories[inventory_id]
+
+    local inventory = {}
+    inventory.inventory_id = inventory_id
+
+    inventories.construct(M, inventory, inventory_data)
+
+    return inventory
+end
+
+--FACTIONS---------------------------------------------------------------------
 
 function M.create_faction()
     local faction_id = #(data.factions) + 1
@@ -105,6 +120,19 @@ function M.create_faction()
     local result = M.get_faction(faction_id)
     return result
 end
+
+function M.get_faction(faction_id)
+    local faction_data = data.factions[faction_id]
+
+    local faction = {}
+    faction.faction_id = faction_id
+
+    factions.construct(M, faction, faction_data)
+
+    return faction
+end
+
+--AVATAR-----------------------------------------------------------------------
 
 function M.set_avatar(character)
     if character == nil then
@@ -121,28 +149,18 @@ function M.get_avatar()
     return M.get_character(data.avatar_id)
 end
 
-function M.abandon()
-    data.rooms = {}
-    data.characters = {}
-    data.factions = {}
-    data.messages = {}
-    data.inventories = {}
-    data.avatar_id = nil
-end
+--MESSAGES---------------------------------------------------------------------
 
-function M.initialize()
-    M.abandon()
-    initializer.initialize(M)
+function M.create_message()
+    local message_id = #(data.messages) + 1
+    data.messages[message_id]={}
+
+    local result = M.get_message(message_id)
+    return result
 end
 
 function M.has_messages()
     return #data.messages > 0
-end
-
-function M.dismiss_message()
-    if M.has_messages() then
-        table.remove(data.messages, 1)
-    end
 end
 
 function M.get_message(message_id)
@@ -156,10 +174,6 @@ function M.get_message(message_id)
     return message
 end
 
-function M.recycle_character(character)
-    data.characters[character.character_id] = nil
-end
-
 function M.get_next_message()
     if M.has_messages() then
         return M.get_message(1)
@@ -167,12 +181,10 @@ function M.get_next_message()
     return nil
 end
 
-function M.create_message()
-    local message_id = #(data.messages) + 1
-    data.messages[message_id]={}
-
-    local result = M.get_message(message_id)
-    return result
+function M.dismiss_message()
+    if M.has_messages() then
+        table.remove(data.messages, 1)
+    end
 end
 
 return M
